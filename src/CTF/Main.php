1003b80ce974c5fcaa0929a2a2af41ddd1aecda5
SavionLegendZzz <saviondeavault@gmail.com>
2015-12-16 07:15:51 -0500

    Update Main.php

--- src/CTF/Main.php
+++ src/CTF/Main.php
@@ -8,6 +8,11 @@ use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
 
 use CTF\GameTask;
 use CTF\kickTask;
@@ -21,6 +26,12 @@ class Main extends PluginBase implements Listener {
         public $redPlayers = [];
         
         public $bluePlayers = [];
+		
+		public $Setter = 0;
+		
+		public $bluePoints = 0;
+		
+		public $redPoints = 0;
     
         public function onEnable() {
                 $this->saveResource("config.yml", false);
@@ -49,6 +60,10 @@ class Main extends PluginBase implements Listener {
         }
 
         public function pickTeam(Player $p) {
+			if(!$this->getConfig()->exsists("Blue-flag-return"){
+				$p->sendMessage("The game isn't set up please set up the match and restart the server!");
+				return;
+			}
                 if(count($this->bluePlayers) < count($this->redPlayers)) {
                         $this->addBluePlayer($p);
                 } elseif(count($this->redPlayers) < count($this->bluePlayers)) {
@@ -69,5 +84,68 @@ class Main extends PluginBase implements Listener {
                 $s = floor($int % 60);
                 return (($m < 10 ? "0" : "") . $m . ":" . ($s < 10 ? "0" : "") . $s);
         }
+		
+		public function gameSet(PlayerInteractEvent $ev){
+		$p = $ev->getPlayer();
+	    $usrname = $p->getName();
+		$block = $ev->getBlock();
+		$levelname = $p->getLevel()->getName();
+		
+		if(isset($this->Setter[$usrname])){
+			switch($this->Setter[$usrname]){
+				
+				case 0:
+				$this->redFlag=array(
+					"x" =>$block->getX(),
+					"y" =>$block->getY(),
+					"z" =>$block->getZ(),
+					"level" =>$levelname);
+				    $this->config->set("Red-flag",$this->redFlag);
+				    $this->config->save();
+					$p->sendMessage("Red flag set, please tap the blue flag!");
+					$this->Setter[$usrname]++;
+					break;
+					
+					
+                case 1:
+				$this->blueFlag=array(
+					"x" =>$block->getX(),
+					"y" =>$block->getY(),
+					"z" =>$block->getZ(),
+					"level" =>$levelname);
+				    $this->config->set("Blue-flag",$this->blueFlag);
+				    $this->config->save();
+					$p->sendMessage("Blue flag set, please tap the red flag return!");
+					break;
+				
+				case 2:
+				$this->redFlagReturn=array(
+					"x" =>$block->getX(),
+					"y" =>$block->getY(),
+					"z" =>$block->getZ(),
+					"level" =>$levelname);
+				    $this->config->set("Red-flag-return",$this->redFlagReturn);
+				    $this->config->save();
+					$p->sendMessage("Red-flag return set, please tap the blue flag return!");
+					break;
+					
+			    case 3:
+				$this->blueFlagReturn=array(
+					"x" =>$block->getX(),
+					"y" =>$block->getY(),
+					"z" =>$block->getZ(),
+					"level" =>$levelname);
+				    $this->config->set("Blue-flag-return",$this->blueFlagReturn);
+				    $this->config->save();
+					$p->sendMessage("Set up is done, you can now play!");
+					unset($this->Setter[$usrname]);
+					break;
+				
+				
+				
+				
+			}
+		}
+		}
 
 }
